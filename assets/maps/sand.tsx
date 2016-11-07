e2b3a40700a7a2dae5a1bb70847955fc4f983424
ranjak <ranjak.hector@gmail.com>
2016-11-07 16:56:53 +0100

    Level 2 done.

---
+++ assets/maps/sand.tsx
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="sand" tilewidth="128" tileheight="128" tilecount="10" columns="3">
+ <tile id="0">
+  <image width="128" height="128" source="../PNG/sand_earth_tiles/grass_earth_38.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="../PNG/sand_earth_tiles/grass_earth_39.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="../PNG/sand_earth_tiles/grass_earth_40.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="../PNG/sand_earth_tiles/grass_earth_43.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="../PNG/sand_earth_tiles/grass_earth_44.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="../PNG/sand_earth_tiles/grass_earth_57.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="../PNG/sand_earth_tiles/grass_earth_58.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="../PNG/sand_earth_tiles/grass_earth_52.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="../PNG/sand_earth_tiles/grass_earth_53.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="128" source="../PNG/sand_earth_tiles/grass_earth_54.png"/>
+ </tile>
+</tileset>
