8300ec604665777929711c1060e06d48ebc723d3
ranjak <ranjak.hector@gmail.com>
2016-11-08 11:53:28 +0100

    Add a third level.

---
+++ assets/maps/rock_sand.tsx
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="rock_sand" tilewidth="128" tileheight="128" tilecount="10" columns="3">
+ <tile id="0">
+  <image width="128" height="128" source="../PNG/sand_stone_tiles/grass_stone_38.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="../PNG/sand_stone_tiles/grass_stone_39.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="../PNG/sand_stone_tiles/grass_stone_40.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="../PNG/sand_stone_tiles/grass_stone_43.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="../PNG/sand_stone_tiles/grass_stone_44.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="../PNG/sand_stone_tiles/grass_stone_57.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="../PNG/sand_stone_tiles/grass_stone_52.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="../PNG/sand_stone_tiles/grass_stone_53.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="128" source="../PNG/sand_stone_tiles/grass_stone_58.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <image width="128" height="128" source="../PNG/sand_stone_tiles/grass_stone_54.png"/>
+ </tile>
+</tileset>
